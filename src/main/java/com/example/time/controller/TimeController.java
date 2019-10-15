4f36d485c833ae6e8a39e3d11aa47fca6c701b6a
sekharrao <m.sekhar.rao@gmail.com>
2019-10-15 16:47:21 +0100

    publish data every 5sec changes.

--- src/main/java/com/example/time/controller/TimeController.java
+++ src/main/java/com/example/time/controller/TimeController.java
@@ -3,7 +3,7 @@ package com.example.time.controller;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
-import org.springframework.scheduling.annotation.Schedules;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,12 +14,19 @@ import com.example.time.model.CustomResponse;
 @CrossOrigin
 public class TimeController {
 
+	
+	private CustomResponse customResponse = new CustomResponse();
+	
 	@GetMapping("/time")
-	@Schedules()
     public CustomResponse greeting() {
-         LocalTime time = LocalTime.now();
-		 DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
-		 return new CustomResponse(time.format(formatter));
+		 return customResponse;
     }
+	
+	@Scheduled(fixedRate = 5000)
+	private void setTime() {
+		LocalTime time = LocalTime.now();
+		 DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+		 customResponse.setTime(time.format(formatter));	
+	}
 
 }
--- src/main/java/com/example/time/model/CustomResponse.java
+++ src/main/java/com/example/time/model/CustomResponse.java
@@ -4,9 +4,5 @@ import lombok.Data;
 
 @Data
 public class CustomResponse {
-
-	private final String time;
-	public CustomResponse(String time){
-		this.time = time;
-	}
+	private String time;
 }
